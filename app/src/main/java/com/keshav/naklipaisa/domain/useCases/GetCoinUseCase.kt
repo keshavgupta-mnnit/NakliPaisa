281d0ae1bdd5e4f9d75ad23bb6aec74b0f8d6405
Keshav <keshavgupta.mnnit@gmail.com>
2022-07-10 01:32:56 +0530

    usecase done

---
+++ app/src/main/java/com/keshav/naklipaisa/domain/useCases/GetCoinUseCase.kt
@@ -0,0 +1,29 @@
+package com.keshav.naklipaisa.domain.useCases
+
+import com.keshav.naklipaisa.common.Resource
+import com.keshav.naklipaisa.data.remote.dto.toCoinDetail
+import com.keshav.naklipaisa.domain.model.CoinDetail
+import com.keshav.naklipaisa.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(private val repository: CoinRepository) {
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId)
+            emit(Resource.Success(coin.toCoinDetail()))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "Unexpected Error Occurred"))
+        } catch (e: IOException) {
+            emit(
+                Resource.Error(
+                    e.localizedMessage ?: "Couldn't reach server. Check Internet connection"
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/keshav/naklipaisa/domain/useCases/GetCoinsUseCase.kt
@@ -0,0 +1,29 @@
+package com.keshav.naklipaisa.domain.useCases
+
+import com.keshav.naklipaisa.common.Resource
+import com.keshav.naklipaisa.data.remote.dto.toCoin
+import com.keshav.naklipaisa.domain.model.Coin
+import com.keshav.naklipaisa.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(private val repository: CoinRepository) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins()
+            emit(Resource.Success(coins.map { it.toCoin() }))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "Unexpected Error Occurred"))
+        } catch (e: IOException) {
+            emit(
+                Resource.Error(
+                    e.localizedMessage ?: "Couldn't reach server. Check Internet connection"
+                )
+            )
+        }
+    }
+}
